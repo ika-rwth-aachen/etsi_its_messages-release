14e28ae3a82b507fab0ff87577476aaa65fa3499
Jean-Pierre <jean-pierre.busch@ika.rwth-aachen.de>
2025-02-17 17:59:42 +0000

    Rebase from 'upstream'

--- etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/cam/cam_setters.h
+++ etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/cam/cam_setters.h
@@ -2,7 +2,7 @@
 =============================================================================
 MIT License
 
-Copyright (c) 2023-2024 Institute for Automotive Engineering (ika), RWTH Aachen University
+Copyright (c) 2023-2025 Institute for Automotive Engineering (ika), RWTH Aachen University
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -30,8 +30,6 @@ SOFTWARE.
  */
 
 #pragma once
-
-#include <etsi_its_msgs_utils/impl/constants.h>
 
 namespace etsi_its_cam_msgs::access {
 
