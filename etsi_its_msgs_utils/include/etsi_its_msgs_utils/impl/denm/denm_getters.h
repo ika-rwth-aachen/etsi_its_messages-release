7ee14fca73a814ea0468dad6d7b40835c73cbd58
Jean-Pierre <jean-pierre.busch@ika.rwth-aachen.de>
2025-02-17 17:58:42 +0000

    Rebase from 'upstream'

--- etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/denm/denm_getters.h
+++ etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/denm/denm_getters.h
@@ -2,7 +2,7 @@
 =============================================================================
 MIT License
 
-Copyright (c) 2023-2024 Institute for Automotive Engineering (ika), RWTH Aachen University
+Copyright (c) 2023-2025 Institute for Automotive Engineering (ika), RWTH Aachen University
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -33,6 +33,7 @@ SOFTWARE.
 
 namespace etsi_its_denm_msgs::access {
 
+#include <etsi_its_msgs_utils/impl/asn1_primitives/asn1_primitives_getters.h>
 #include <etsi_its_msgs_utils/impl/cdd/cdd_v1-3-1_getters.h>
 
 /**
