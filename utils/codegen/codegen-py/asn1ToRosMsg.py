97fe838e733ceb6103ff2526e8ae0d23b8eb16a2
Jean-Pierre <jean-pierre.busch@ika.rwth-aachen.de>
2025-02-17 17:59:27 +0000

    Rebase from 'upstream'

--- utils/codegen/codegen-py/asn1ToRosMsg.py
+++ utils/codegen/codegen-py/asn1ToRosMsg.py
@@ -3,7 +3,7 @@
 # ==============================================================================
 # MIT License
 #
-# Copyright (c) 2023-2024 Institute for Automotive Engineering (ika), RWTH Aachen University
+# Copyright (c) 2023-2025 Institute for Automotive Engineering (ika), RWTH Aachen University
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -138,10 +138,21 @@ def findDependenciesOfRosMessageType(parent_file_path: str, file_list: List[str]
                     new_file_list.append(msg_type)
                     new_file_list = findDependenciesOfRosMessageType(f"{os.path.dirname(parent_file_path)}/{msg_type}.msg", new_file_list)
 
-    # make sure there are no duplicates and sort alphabetically
-    new_file_list = sorted(list(set(new_file_list)))
-
     return new_file_list
+
+def additionalMessageTypes(path: str, msg_type: str) -> List[str]:
+    additional = []
+
+    for f in glob.glob(os.path.join(path, "*.msg")):
+        msg_filename = os.path.splitext(os.path.basename(f))[0]
+        if msg_type == "DENM" and msg_filename.endswith("SubCauseCode"):
+            additional.append(msg_filename)
+
+    return additional
+
+def sortMessageFiles(files: List[str]) -> List[str]:
+    # make sure there are no duplicates and sort alphabetically
+    return sorted(list(set(files)))
 
 def generateCMakeLists(msg_files: list, file_path: str, type: str) -> None:
     with open(file_path, "w") as f:
@@ -226,6 +237,8 @@ def main():
         msg_type = "CollectivePerceptionMessage"
     elif args.type == "cam_ts":
         msg_type = "CAM"
+    elif args.type == "denm_ts":
+        msg_type = "DENM"
     elif args.type == "mapem_ts":
         msg_type = "MAPEM"
     elif args.type == "spatem_ts":
@@ -233,11 +246,15 @@ def main():
     elif args.type == "vam_ts":
         msg_type = "VAM"
     msg_files = findDependenciesOfRosMessageType(os.path.join(args.output_dir, f"{msg_type}.msg"), [msg_type])
+    msg_files += additionalMessageTypes(args.output_dir, msg_type)
+    msg_files = sortMessageFiles(msg_files)
+
     print("Generating CMakeLists.txt ...")
     generateCMakeLists(msg_files, os.path.join(args.output_dir, "../CMakeLists.txt"), args.type)
     print("Removing files not required for top-level message type ...")
     for f in glob.glob(os.path.join(args.output_dir, "*.msg")):
-        if os.path.splitext(os.path.basename(f))[0] not in msg_files:
+        msg_filename = os.path.splitext(os.path.basename(f))[0]
+        if msg_filename not in msg_files:
             os.remove(f)
 
     print(f"Generated {len(msg_files)} ROS .msg files for {msg_type}")
