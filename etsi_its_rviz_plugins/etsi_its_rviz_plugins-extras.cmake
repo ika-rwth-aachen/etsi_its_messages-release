e1d2b00852c8d4208432a2d39170b442ef361d93
Jean-Pierre <jean-pierre.busch@ika.rwth-aachen.de>
2025-05-16 13:00:20 +0000

    Trimmed the branch to only the etsi_its_conversion/etsi_its_denm_conversion sub directory

--- etsi_its_rviz_plugins/etsi_its_rviz_plugins-extras.cmake
+++
@@ -1,3 +0,0 @@
-# find package Qt5 because otherwise using the etsi_its_msgs_rviz_plugins:etsi_its_msgs_rviz_plugins
-# exported target will complain that the Qt5::Widgets target does not exist
-find_package(Qt5 REQUIRED QUIET COMPONENTS Widgets)
